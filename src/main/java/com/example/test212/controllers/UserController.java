863e6a2522e44590aa2daf36e7e8bc6abcc51675
Forichok <nnudalov@gmail.com>
2022-04-27 20:07:06 +0300

    cors setup

--- src/main/java/com/example/test212/controllers/UserController.java
+++ src/main/java/com/example/test212/controllers/UserController.java
@@ -1,9 +1,27 @@
 package com.example.test212.controllers;
 
+import com.example.test212.controllers.exceptions.StudentNotExistException;
+import com.example.test212.controllers.models.UserDto;
+import com.example.test212.database.entities.User;
+import com.example.test212.database.repositories.UserRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/private/users")
 public class UserController {
+
+    private final UserRepository userRepo;
+
+    public UserController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable("id") String id) {
+        Optional<User> user = userRepo.findById(id);
+        return user.map(UserDto::new).orElse(null);
+    }
 
 }
--- src/main/java/com/example/test212/controllers/models/UserDto.java
+++ src/main/java/com/example/test212/controllers/models/UserDto.java
@@ -1,9 +1,25 @@
 package com.example.test212.controllers.models;
 
+import com.example.test212.database.entities.User;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
 
 @Data
+@NoArgsConstructor
 public class UserDto {
 
     private String id;
+    private int age;
+    private String name;
+
+    private String email;
+
+    public UserDto(User user) {
+        id = user.getId();
+        age = user.getAge();
+        name = user.getName();
+        email = user.getEmail();
+    }
 }
